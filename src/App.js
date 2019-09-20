80c2f329d5e1bb9d3a9e08e0cae79a6fe6229f8c
Tuan Nguyen <tuanng@pixiostudio.com>
2019-09-20 11:18:27 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,110 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+ 
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ 
+class App extends React.Component {
+ 
+  render()  {
+    return  (
+      <BrowserRouter>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/topics">Topics</Link>
+            </li>
+          </ul>
+ 
+          <hr />
+          <div className="main-route-place">
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/topics" component={Topics} />
+          </div>
+        </div>
+      </BrowserRouter>
+    );
+  }
+ 
 }
-
+ 
+class Home extends React.Component {
+ 
+  render()  {
+    return (
+      <div>
+        <h2>Home</h2>
+      </div>
+    );
+  }
+}
+ 
+class About  extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>About</h2>
+      </div>
+    );
+  }
+}
+ 
+class Topics extends React.Component {
+  render( ) {
+    return (
+      <div>
+        <h2>Topics</h2>
+        <ul>
+          <li>
+            <Link to={`${this.props.match.url}/rendering`}>
+              Rendering with React
+            </Link>
+          </li>
+          <li>
+            <Link to={`${this.props.match.url}/components`}>Components</Link>
+          </li>
+          <li>
+            <Link to={`${this.props.match.url}/props-v-state`}>
+              Props v. State
+            </Link>
+          </li>
+        </ul>
+ 
+        <div className="secondary-route-place">
+          <Route
+            path={`${this.props.match.url}/:topicId`}
+            component={Topic} />
+          <Route
+            exact
+            path={this.props.match.url}
+            render={() =>
+              <h3>
+                Please select a topic.
+              </h3>
+            }
+            />
+        </div>
+      </div>
+    );
+  }
+}
+ 
+class Topic extends React.Component {
+  render()  {
+    return (
+      <div>
+        <h3>
+          {this.props.match.params.topicId}
+        </h3>
+      </div>
+    );
+  }
+}
+ 
 export default App;
